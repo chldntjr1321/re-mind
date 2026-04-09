6294480c7f1643c5ab9bad40bd3303498f0ecbc9
최우석 <nick501@naver.com>
2026-04-09 14:45:23 +0900

    chore: JSX 작은따옴표 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="h-full">
-      <body className="min-h-full flex flex-col font-sans antialiased">{children}</body>
+    <html lang='ko' className='h-full'>
+      <body className='min-h-full flex flex-col font-sans antialiased'>{children}</body>
     </html>
   );
 }
